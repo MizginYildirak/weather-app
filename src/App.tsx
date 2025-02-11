14064d606f71904d327e6ca07ea0a4743143e6af
MizginYildirak <mizgin_yldrak@hotmail.com>
2025-02-11 13:35:50 +0300

    get weather data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import SearchSection from "./components/SearchSection";
 import CurrentWeather from "./components/CurrentWeather";
 import HourlyWeatherItem from "./components/HourlyWeatherItem";
-import "./index.css"
+import "./index.css";
 
 const App = () => {
+  //Fetches weather details based on the API URL
+  const getWeatherDetails = async (API_URL) => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json()
+      console.log("data", data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="container">
-      <SearchSection />
+      <SearchSection getWeatherDetails={getWeatherDetails} />
 
       <div className="weather-section">
         <CurrentWeather />
-        
+
         <div className="hourly-forecast">
           <ul className="weather-list">
             <HourlyWeatherItem />
--- src/components/SearchSection.tsx
+++ src/components/SearchSection.tsx
@@ -1,14 +1,22 @@
 import React, { useRef, FormEvent } from "react";
 import "../index.css";
 
-const SearchSection: React.FC = () => {
+interface SearchSectionProps {
+  getWeatherDetails: (url: string) => void;
+}
+
+const SearchSection: React.FC<SearchSectionProps> = ({ getWeatherDetails }) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   const handleCitySearch = (e: FormEvent) => {
     e.preventDefault();
-    if (inputRef.current) {
-      console.log("inputCurrentvalue:", inputRef.current.value);
-    }
+    const inputValue = inputRef.current?.value || "";
+    console.log("inputValue:", inputValue);
+
+    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${inputValue}`;
+    getWeatherDetails(API_URL);
   };
 
   return (
